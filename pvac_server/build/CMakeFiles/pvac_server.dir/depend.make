fda28d500b14931f608b4abdc80939b5f7c7934e
xme <payment.adsku@gmail.com>
2026-04-20 13:02:58 +0700

    Remove pvac_server/build/* from Git tracking

--- pvac_server/build/CMakeFiles/pvac_server.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pvac_server.
-# This may be replaced when dependencies are built.
--- pvac_server/build/CMakeFiles/pvac_server.dir/lib/tweetnacl.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/pvac_server.dir/lib/tweetnacl.c.obj: \
- C:\Users\Administrator\Documents\Devs\Octra\OctWa\pvac_server\lib\tweetnacl.c \
- C:\Users\Administrator\Documents\Devs\Octra\OctWa\pvac_server\lib\tweetnacl.h
